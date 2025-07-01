01fe8268c1796b289ecf2c7f4a44ac2c4f57b55a
Luscyn <imcayanan.creatives@gmail.com>
2025-07-02 05:05:15 +0800

    Test Files Only

---
+++ test.js
@@ -0,0 +1,11 @@
+// const summary = (name, ave, grade, guardian, contact) => { 
+//     return `${name} got a ${grade} (${ave}). Guardian: ${guardian} (${contact})`  
+// }
+
+// console.log(summary("test", "test", "test", "test", "test"));
+
+const arr = [1,2,3,"asd"];
+const arr2 = [1,2,3];
+
+console.log(isNaN(arr));
+console.log(isNaN(arr2));
